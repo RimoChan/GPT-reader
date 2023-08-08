dbf33cc4080f8a4fc3d8a5111a5a4c34ef8a6a6c
RimoChan <RimoChan@stu.xmu.edu.cn>
2023-08-08 21:35:16 +0800

    优化prompt

--- read.py
+++ read.py
@@ -1,4 +1,5 @@
 import time
+import logging
 import chardet
 
 from rimo_storage.cache import disk_cache
@@ -8,18 +9,19 @@ openai.api_key = 'xxxxxx'
 
 
 @disk_cache()
-def 问(s, a, temperature):
+def 问(s, u, a, temperature):
     messages = [
         {"role": "system", "content": s},
-        {"role": "user", "content": a},
+        {"role": "user", "content": u},
+        {"role": "assistant", "content": a},
     ]
     chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, temperature=temperature, max_tokens=300)
     return chat.choices[0].message.content
 
 
-def 超问(s, a):
-    for i in [0, 0.3, 0.6, 0.9, 0.92, 0.94, 0.96, 0.98, 1]:
-        r = 问(s, a, i)
+def 超问(s, u, a):
+    for temperature in [0, 0.3, 0.6, 0.9, 0.92, 0.94, 0.96, 0.98, 1]:
+        r = 问(s, u, a, temperature=temperature)
         if r[-1] == '。':
             return r
     return r
@@ -54,11 +56,12 @@ def 读(fname):
             别.append(line)
         else:
             别[-1] += line
-    头 = '你是一个小说压缩器。用户每次会输入一段约2000字的小说，你需要将这段话缩写到200字以下。你应当忽略输入中的章节名和卷名等meta信息。你不能直接引用人物对话，而要进行总结。'
+    头 = '你是一个小说压缩器。用户每次会输入一段约2000字的小说，你需要将这段话缩写到200字以下。\n你应当忽略输入中的章节名和卷名等meta信息。\n你不能直接引用人物对话，而要进行总结。\n你应该尽可能使用名字指代每个人物，除非你不知道名字。'
     过去的剧情 = []
-    for i, a in enumerate(别):
+    for i, u in enumerate(别):
         if i == 0:
             s = f'{头}'
+            a = '本部分的剧情如下:'
         else:
             过去剧情的切片 = ''
             for j in 过去的剧情[::-1]:
@@ -67,14 +70,14 @@ def 读(fname):
                 else:
                     break
             if i > 0:
-                s = f'{头}\n\n以下是之前的剧情: \n{过去剧情的切片}\n你不需要输出之前的剧情，只需输出用户输入部分的剧情。'
-
+                s = f'{头}\n\n\n以下是之前的剧情: \n{过去剧情的切片}\n你不需要输出之前的剧情，只需总结新增部分的剧情。'
+            a = '本部分的新增剧情如下:'
         print('=====================================')
         print(s)
         print('-------------------------------------')
         while True:
             try:
-                r = 超问(s, a)
+                r = 超问(s, u, a)
             except Exception as e:
                 print('Exception:', e)
                 time.sleep(10)
